533c01526426475ba9327c8c27f1e4306050eb85
Imanol <imanol.nebreda.dev@gmail.com>
2022-01-25 14:08:08 +0100

    Update extension.ts
    Disable mode after executing the command automatically

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,43 @@
-import { commands, window, workspace, ExtensionContext } from 'vscode';
+import { commands, ExtensionContext, window, workspace } from "vscode";
 const { registerCommand, executeCommand } = commands;
 const { showInformationMessage } = window;
 
-
-const commandForLetter = (letter: string) => (
-  (workspace
-    .getConfiguration('keybindingMode')
-    .get('letterCommandMapping', <string[]>[])
-    .find(letterCommandMappingString => letterCommandMappingString[0] === letter) || ''
-  )
-  .split(',')[1]
-);
+const commandForLetter = (letter: string) =>
+  (
+    workspace
+      .getConfiguration("keybindingMode")
+      .get("letterCommandMapping", <string[]>[])
+      .find(letterCommandMappingString => letterCommandMappingString[0] === letter) || ""
+  ).split(",")[1];
 
 export function activate(context: ExtensionContext) {
-  let enabled = false;
+  // let enabled = false;
 
-  context.subscriptions.push(registerCommand('keybindingMode.toggle', () => {
-    enabled = !enabled;
-    executeCommand('setContext', 'keybindingMode:enabled', enabled);
-    showInformationMessage(`keybindingMode ${enabled ? 'enabled' : 'disabled'}`);
-  }));
+  context.subscriptions.push(
+    registerCommand("keybindingMode.toggle", () => {
+      // enabled = !enabled;
+      // executeCommand('setContext', 'keybindingMode:enabled', enabled);
+      // showInformationMessage(`keybindingMode ${enabled ? 'enabled' : 'disabled'}`);
+      executeCommand("setContext", "keybindingMode:enabled", true);
+      showInformationMessage("keybindingMode enabled");
+    })
+  );
 
-  context.subscriptions.push(registerCommand('keybindingMode.handleKey', ({ text: letter }) => {
-    if (!enabled) return;
+  context.subscriptions.push(
+    registerCommand("keybindingMode.handleKey", ({ text: letter }) => {
+      const command = commandForLetter(letter);
+      if (command) {
+        executeCommand(command);
+      }
+    })
+  );
 
-    const command = commandForLetter(letter);
-    if (command) {
-      executeCommand(command);
-    }
-  }));
+  context.subscriptions.push(
+    registerCommand("keybindingMode.toggle", () => {
+      executeCommand("setContext", "keybindingMode:disabled", true);
+      showInformationMessage("keybindingMode disabled");
+    })
+  );
 }
 
 export function deactivate() {}
